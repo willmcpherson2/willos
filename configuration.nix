6a29eaa33bfe2f7c174b45fd1cd447853a6b8eaf
William McPherson <willmcpherson2@gmail.com>
2022-11-20 14:41:30 +1100

    Start adding key bindings

--- configuration.nix
+++ configuration.nix
@@ -40,6 +40,9 @@ in
 
   home-manager.users.will = { pkgs, ... }: {
     home.packages = with pkgs; [
+      wofi
+      wofi-emoji
+
       # gnome
       gnome-console
       gnome-connections
@@ -164,6 +167,23 @@ in
       };
       "org/gtk/settings/file-chooser" = {
         clock-format = "12h";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        ];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        name = "run";
+        binding = "<Super>r";
+        command = "wofi --show drun";
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        name = "emoji";
+        binding = "<Super>e";
+        command = "wofi-emoji";
       };
     };
   };
