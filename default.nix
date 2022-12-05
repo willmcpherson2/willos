1ec170df5ddb9bef0a49cff70d7f29bdd5b48440
William McPherson <willmcpherson2@gmail.com>
2022-12-05 15:59:49 +1100

    Use p1 suffix instead of just 1

--- default.nix
+++ default.nix
@@ -16,16 +16,16 @@ pkgs.mkShell {
       parted "$DISK" -- mkpart primary linux-swap -8GB 100%
       parted "$DISK" -- mkpart ESP fat32 1MB 512MB
       parted "$DISK" -- set 3 esp on
-      mkfs.ext4 -L nixos "$DISK"1
-      mkswap -L swap "$DISK"2
-      mkfs.fat -F 32 -n boot "$DISK"3
+      mkfs.ext4 -L nixos "$DISK"p1
+      mkswap -L swap "$DISK"p2
+      mkfs.fat -F 32 -n boot "$DISK"p3
       mount /dev/disk/by-label/nixos /mnt
       mkdir -p /mnt/boot
       for _ in {1..3}
       do
         mount /dev/disk/by-label/boot /mnt/boot && break || sleep 1
       done
-      swapon "$DISK"2
+      swapon "$DISK"p2
 
       nixos-generate-config --root /mnt
       cp -r ${willos}/* /mnt/etc/nixos
