e1ddce8993fd1b8424c0ba3cd8cf58705b94850e
William McPherson <willmcpherson2@gmail.com>
2022-12-06 19:22:55 +1100

    Use AusE
    Configuring this is confusing...

--- dot/user.js
+++ dot/user.js
@@ -3,6 +3,7 @@ user_pref("browser.download.always_ask_before_handling_new_types", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("browser.uiCustomization.state", '{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","history-panelmenu","downloads-button","ublock0_raymondhill_net-browser-action","preferences-button","fxa-toolbar-menu-button","urlbar-container"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"currentVersion":18}');
 user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("intl.locale.requested", "en-au,en-gb,en");
 user_pref("font.default.x-western", "sans-serif");
 user_pref("font.name.sans-serif.x-western", "Liberation Sans");
 user_pref("font.name.serif.x-western", "Liberation Serif");
