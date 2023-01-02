a33ab7ad6382232d6df9b7f93781c69f19c15359
William McPherson <willmcpherson2@gmail.com>
2023-01-02 15:49:21 +1100

    Use auto-dark

--- dot/doom/config.el
+++ dot/doom/config.el
@@ -1,8 +1,15 @@
-(setq doom-theme 'doom-one-light)
 (let ((font "JetBrainsMono Nerd Font 16"))
   (setq doom-font font
         doom-variable-pitch-font font
         doom-serif-font font
         doom-unicode-font font
         doom-big-font font))
+
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(use-package! auto-dark
+  :init
+  (require 'dbus)
+  (setq auto-dark-dark-theme 'doom-one)
+  (setq auto-dark-light-theme 'doom-one-light)
+  (auto-dark-mode t))
