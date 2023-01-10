53aa00cbafac928d3289e8fe14c8ab7e8ddc187b
William McPherson <willmcpherson2@gmail.com>
2023-01-10 22:14:12 +1100

    Configure Emacs

--- dot/emacs.el
+++ dot/emacs.el
@@ -1,9 +1,24 @@
+;; TODO
+;; fix window placement and temporary buffer stuff
+;; too many buffers on startup
+;; projects
+;; company not showing icons
+;; add company backends
+;; evil mode feels anti-emacs -- try god mode, meow, etc.
+;; editing & pasting in any input (command input)
+;; copying any text
+;; eventually start mapping keys
+;; need good shell/term solution (i want shell to be text buffer)
+;; treesitter, lsp, language modes (flycheck?)
+
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (menu-bar-mode -1)
-(set-fringe-mode 10)
+(set-fringe-mode 4)
 (pixel-scroll-precision-mode 1)
+(global-display-line-numbers-mode 1)
+(column-number-mode 1)
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 160)
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (setq inhibit-startup-message t
@@ -14,9 +29,36 @@
 
 (setq use-package-always-ensure t)
 
-(use-package no-littering)
+(use-package no-littering
+  :custom
+  (auto-save-file-name-transforms
+   `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+
+(use-package all-the-icons
+  :if
+  (display-graphic-p))
+
+(use-package all-the-icons-completion
+  :after
+  (marginalia all-the-icons)
+  :hook
+  (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-one t)
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :init
+  (doom-modeline-mode 1))
 
 (use-package auto-dark
+  :custom
+  (auto-dark-dark-theme 'doom-one)
+  (auto-dark-light-theme 'doom-one-light)
   :init
   (require 'dbus)
   (auto-dark-mode t))
@@ -65,7 +107,13 @@
   (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package company
-  :init
-  (global-company-mode)
   :custom
-  (company-selection-wrap-around t))
+  (company-selection-wrap-around t)
+  :config
+  (global-company-mode t))
+
+(use-package magit)
+
+(use-package diff-hl
+  :init
+  (global-diff-hl-mode))
