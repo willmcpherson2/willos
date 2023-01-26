3b364ca88dafc609be84432123eb7f4cca4fb3aa
William McPherson <willmcpherson2@gmail.com>
2023-01-26 21:14:31 +1100

    Don't close Firefox when closing last tab

--- dot/user.js
+++ dot/user.js
@@ -13,3 +13,4 @@ user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.zoom.full", false);
 user_pref("reader.content_width", 8);
 user_pref("extensions.pocket.enabled", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
