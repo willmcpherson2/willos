b908fe64b42d153276564329c778a52f271df0f7
William McPherson <willmcpherson2@gmail.com>
2023-09-05 14:53:59 +1000

    Fix mime issues

--- configuration.nix
+++ configuration.nix
@@ -247,8 +247,20 @@ in
     xdg.mimeApps = {
       enable = true;
       defaultApplications = {
-        "image/png" = [ "firefox.desktop" ];
-        "application/pdf" = [ "firefox.desktop" ];
+        "application/pdf" = "firefox.desktop";
+        "application/x-extension-htm" = "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/x-extension-xht" = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "image/jpeg" = "firefox.desktop";
+        "image/png" = "firefox.desktop";
+        "text/html" = "firefox.desktop";
+        "text/uri-list" = "firefox.desktop";
+        "x-scheme-handler/chrome" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
       };
     };
 
