7d50ff5ebd8ef42ca2ce23c5e579bdc383671bc6
William McPherson <willmcpherson2@gmail.com>
2023-11-29 16:07:42 +1100

    Inline nix.nix

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,20 @@ let
     "https://github.com/nix-community/home-manager/archive/release-${version}.tar.gz";
 in
 {
-  nix = import ./nix.nix;
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+    ];
+  };
 
   system.stateVersion = version;
 
