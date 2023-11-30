9ffdb16ec25b52dccb7dfb44e7fe9ee52de5e1f9
William McPherson <willmcpherson2@gmail.com>
2023-11-30 13:59:30 +1100

    Remove wl-copy from script

--- will/bin/new-ssh-key.sh
+++ will/bin/new-ssh-key.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 ssh-keygen -t ed25519 -C "willmcpherson2@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub | wl-copy
+cat ~/.ssh/id_ed25519.pub
