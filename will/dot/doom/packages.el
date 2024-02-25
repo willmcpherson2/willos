8ce6ab69b9b351582bf0b9b051197217d05125fd
William McPherson <willmcpherson2@gmail.com>
2024-02-25 13:06:30 +1100

    Pin some Emacs packages

--- will/dot/doom/packages.el
+++ will/dot/doom/packages.el
@@ -3,9 +3,11 @@
 (package! auto-dark)
 
 (package! copilot
-  :recipe (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el")))
+  :pin "fd4d7e8c1e95aa9d3967b19905c9b8c3e03f6a5c"
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")))
 
 (package! async-await)
 (package! websocket)
 (package! aichat
+  :pin "1b3270c96080a67389f6089d489e883c8367e6f8"
   :recipe (:host github :repo "xhcoding/emacs-aichat" :files ("*")))
