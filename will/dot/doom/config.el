b0c927b9cdfdeb49471fc1c334b96f4ca6417007
William McPherson <willmcpherson2@gmail.com>
2024-07-17 18:59:30 +1000

    Reorganise setq

--- will/dot/doom/config.el
+++ will/dot/doom/config.el
@@ -5,24 +5,20 @@
           mac-control-modifier 'meta
           auto-dark-allow-osascript t))
 
-(setq-default tab-width 2)
-(setq display-line-numbers-type 'relative
-      doom-font (font-spec :family "JetBrains Mono" :size 22))
-
 (setq auto-dark-dark-theme 'doom-one
       auto-dark-light-theme 'doom-one-light)
 (auto-dark-mode 1)
 
-(setq evil-want-minibuffer t
-      evil-goggles-duration 0.5)
+(setq-default tab-width 2)
 
-(setq js-indent-level 2)
-
-(setq lsp-restart 'ignore)
-
-(setq projectile-switch-project-action #'projectile-dired)
-
-(setq markdown-gfm-use-electric-backquote nil)
+(setq display-line-numbers-type 'relative
+      doom-font (font-spec :family "JetBrains Mono" :size 22)
+      evil-want-minibuffer t
+      evil-goggles-duration 0.5
+      js-indent-level 2
+      lsp-restart 'ignore
+      projectile-switch-project-action #'projectile-dired
+      markdown-gfm-use-electric-backquote nil)
 
 (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
 
