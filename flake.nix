de2a55536702817f36f47ce5c32803028925ab59
William McPherson <willmcpherson2@gmail.com>
2024-07-17 20:52:22 +1000

    Use short hashes

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-hardware.url = "github:nixos/nixos-hardware/7b49d3967613d9aacac5b340ef158d493906ba79";
-    rust-overlay.url = "github:oxalica/rust-overlay/260ff391290a2b23958d04db0d3e7015c8417401";
+    nixos-hardware.url = "github:nixos/nixos-hardware/7b49d39";
+    rust-overlay.url = "github:oxalica/rust-overlay/260ff39";
     nix-doom-emacs-unstraightened.url = "github:marienz/nix-doom-emacs-unstraightened/379dbd4";
   };
 
