5f12478ebfc3034001b2164883b6c08e06914e90
William McPherson <willmcpherson2@gmail.com>
2024-12-31 17:30:28 +1100

    Remove custom toolbar stuff from Firefox

--- will/dot/user.js
+++ will/dot/user.js
@@ -1,7 +1,6 @@
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("browser.download.always_ask_before_handling_new_types", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
-user_pref("browser.uiCustomization.state", '{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","history-panelmenu","downloads-button","ublock0_raymondhill_net-browser-action","preferences-button","fxa-toolbar-menu-button","urlbar-container"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"currentVersion":18}');
 user_pref("ui.key.menuAccessKeyFocuses", false);
 user_pref("intl.locale.requested", "en-au,en-gb,en");
 user_pref("font.default.x-western", "sans-serif");
