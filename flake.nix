41d397a26a8801e9a4863101b034e6cd0ef227de
William McPherson <willmcpherson2@gmail.com>
2026-03-17 22:11:40 +1100

    Upgrade nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Will's OS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     rust-overlay = {
